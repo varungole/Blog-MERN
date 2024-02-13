8daaba0551669d31f71bf1bf1944bb5f3256cacb
Varun Gole <varungole1999@gmail.com>
2024-02-13 12:45:45 -0500

    login and signup logic

--- frontend/src/components/Login.js
+++ frontend/src/components/Login.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import GoogleIcon from './google-icon.png'
 import LinkedinIcon from './linkedin-icon.png'
 import { useNavigate } from 'react-router-dom';
@@ -10,13 +10,41 @@ function Login() {
     const handleSignIn = () => {
         navigate('/signin')
     }
+
+    const[username , getUsername] = useState('');
+    const[password , getPassword] = useState('');
+
+    const handleLogin = async(e) => {
+      e.preventDefault();
+      const test = await fetch('http://localhost:4050/users');
+      const data = await test.json();
+      // Assuming `data` is an array of user objects
+      const usernameToCheck = username;  // Replace with the username you want to check
+      let flag = false;
+      for (let i = 0; i < data.length; i++) {
+        let tempUser = data[i];
+        if (tempUser.username === usernameToCheck && tempUser.password === password) {
+          flag = true;
+          navigate('/');
+        }
+      }
+
+      if(flag === false)
+      {
+        alert('Incorrect username or password');
+
+      return;
+      }
+
+    }
+
   return (
     <div className='login-page'>
         <div className='username-area'>
             <h1 className='Login-Title'>Login</h1>
-            <input type='text' id='username' className='username'></input>
-            <input type='password' id='password' className='password'></input>
-            <button className='login-button'>Login</button>
+            <input type='text' id='username' className='username' value={username} onChange={(e) => getUsername(e.target.value)}></input>
+            <input type='password' id='password' className='password' value={password} onChange={(e) => getPassword(e.target.value)}></input>
+            <button className='login-button' onClick={handleLogin}>Login</button>
             <h4>Do not have an account?<button className='sign-in' onClick={handleSignIn}>Sign in</button> </h4>
             
             <h4 className='or'>Or</h4>
--- frontend/src/components/Signin.js
+++ frontend/src/components/Signin.js
@@ -4,16 +4,56 @@ import { useNavigate } from 'react-router-dom';
 function Signin() {
   const navigate = useNavigate();
 
+
   const handleLogin = () => {
     navigate('/login');
   };
 
+    const handleSubmit = async (e) => {
+      e.preventDefault();
+      const test = await fetch('http://localhost:4050/users');
+      const data = await test.json();
+
+      // Assuming `data` is an array of user objects
+      const usernameToCheck = username;  // Replace with the username you want to check
+    
+      for (let i = 0; i < data.length; i++) {
+        let tempUser = data[i];
+        if (tempUser.username === usernameToCheck) {
+          alert('User already exists');
+          return;
+        }
+      }
+      const response = await fetch('http://localhost:4050/users', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          username: username,
+          password: password,
+        }),
+      });
+
+      if (response.ok) {
+        navigate("/login");
+      } else {
+        console.log('Not successful');
+      }
+
+      setUsername('')
+      setPassword('')
+      setRepassword('')
+    };
+
+  const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
   const [repassword, setRepassword] = useState('');
 
   const passwordsMatch = password === repassword;
 
   return (
+    <div>
     <div className='signin'>
       <h1 className='sigin-text'>Sign In</h1>
       <input
@@ -21,6 +61,8 @@ function Signin() {
         id='username'
         className='signin-username'
         placeholder='Username'
+        value={username}
+        onChange={(e) => setUsername(e.target.value)}
       />
       <input
         type='password'
@@ -33,24 +75,34 @@ function Signin() {
       <h4 className='reenter'>Re-enter the password</h4>
       <input
         type='password'
-        id='password'
+        id='rePassword'
         className='signin-password'
         value={repassword}
         onChange={(e) => setRepassword(e.target.value)}
         placeholder='Re-enter Password'
       />
       {!passwordsMatch && <h4>Passwords do not match</h4>}
-      <button className='login-button' onClick={handleLogin}>
+      <button className='login-button' onClick={handleSubmit}>
         Sign In
       </button>
       <h4>
         Already have an account?
-        <button className='sign-in' onClick={handleLogin}>
+        <button className='sign-in' type='submit' onClick={handleLogin}>
           Login
         </button>
       </h4>
     </div>
+    </div>
   );
+
+
+
+
+
+  
 }
+
+
+
 
 export default Signin;
